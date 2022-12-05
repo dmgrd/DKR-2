70514bd14f533f488030f3c53d3d40163005b8f2
dmgrd <117730998+dmgrd@users.noreply.github.com>
2022-12-05 14:21:58 +0300

    Add files via upload

---
+++ DKR 2 option 2 code 1.pas
@@ -0,0 +1,23 @@
+﻿Program dkr1option2code1;
+begin
+  var f, b, a, c, j, k, full: integer; 
+  var chisla: array [1..5] of integer;
+  b := 5;
+  k := 0;
+  print('Введите массив:');
+  for var i := 1 to 5
+    do
+    read(chisla[i]);
+  for var i := 1 to b - 2 do
+  begin
+    if (chisla[i] + chisla[i + 1]) = chisla[i + 2] then
+      continue
+    else
+      k += 1;
+    break
+  end;
+  if k = 1 then
+    print('Не удовлетворяет условию')
+  else
+    print('Удовлетворяет условию');
+end.
---
+++ DKR 2 option 2 code 2.pas
@@ -0,0 +1,13 @@
+﻿begin
+  var i:integer;
+  var f:boolean;
+  var s:string;
+writeln('Введите строку для проверки на палиндром:');
+readln(s);
+i:=1;
+f:=true;
+while(i<=length(s) div 2)and f do
+if s[i]<>s[length(s)+1-i] then f:=false
+else i:=i+1;
+if f then print('Палиндром') else print('Не палиндром');
+end.
